c20fc7c3995d3d06df07e181655e97ba4f950ae0
aqdasilva <antqdasilva@gmail.com>
2023-01-25 19:06:05 -0500

    antd.css fizx it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar.jsx';
+import Homepage from './components/Homepage';
+import Exchanges from './components/Exchanges';
+import CryptoCurrencies from './components/CryptoCurrencies';
+import CryptoDetails from './components/CryptoDetails';
+import News from './components/News';
 import './App.css';
+import Layout from 'antd/es/layout/layout';
+import { Switch } from 'antd';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -10,7 +18,27 @@ const App = () => {
         <Navbar />
       </div>
       <div className='main'>
-
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges/>
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <CryptoCurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
       <div className='footer'>
 
